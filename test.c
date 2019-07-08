7e72e086891ba2ae18ce5c0bac80e6cc1ccd9a58
kwenzheng <51584525+kwenzheng@users.noreply.github.com>
2019-07-08 14:23:08 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,45 @@
-#include<stdio.h>
-int main(int argc, const char *argv[])
+char *s_gets(char *st,int n)
 {
-	printf("develop\n");
-	printf("东风吹 战鼓擂\n");
-	printf("hello world\n");
-	printf("高三三班怕过谁");
-        return 0;
+	char *ret_val;
+	char *find;
+	ret_val=fgets(st,n,stdin);
+	if(ret_val)
+	{
+		find=strchr(st,'\n');
+		if(find)
+			*find='\0';
+		else
+			while(getchar()!='\n')
+				continue;
+	}
+	return ret_val;
 }
+void append(FILE *source,FILE *dest)
+{
+	size_t bytes;
+	static char temp[BUFSIZE];
+	while((bytes=fread(temp,sizeof(char),BUFSIZE,source))>0)
+		fwrite(temp,sizeof(char),bytes,dest);
+}
+int main(void)
+{
+	FILE *fa,*fs;//fa指向目标文件，fs指向源文件
+	int files=0;
+	char file_app[SLEN];//目标文件名
+	char file_src[SLEN];//源文件名
+	int ch;
+	s_gets(file_app,SLEN);
+	if((fa=fopen(file_app,"a+"))==NULL)
+	{
+		
+
+
+
+
+
+
+
+
+
+
 
